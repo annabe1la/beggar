0b2bcf3169acba1426df1671ba955fe007daf787
annabe1la <745283555@qq.com>
2018-08-27 19:25:55 +0800

    bg

--- pages/myFriend/myFriend.js
+++ pages/myFriend/myFriend.js
@@ -1,80 +1,80 @@
-// pages/donate/donate.js
-const app = getApp();
+// pages/myFriend/myFriend.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-      isLike: false,
-      heartList: [],
-      value:'',
+      inputLength:0,
   },
-  onClick: function () {
-    this.setData({
-        isLike:!this.data.isLike,
-        
-    })
-  },
-  onChange:function(e){
-      let v = e.detail.value;
-        this.setData({
-            isLike:v==0?true:false
-        })
-  },
+  watchWords:function(event){
+      this.setData({
+          inputLength: event.detail.cursor
+      })
+    },
+    onShow:function(){
+        console.log("shaa");
+        wx.showLoading({
+            title: '等一哈',
+            mask: true
+        });
+        setTimeout(function () {
+            wx.hideLoading()
+        }, 2000)
+    },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+  
   },
-
+    
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+  
   }
 })
