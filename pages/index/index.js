77281019c720175f84225da04d294d8e68e2f5c2
AnNabe1la <shijin@AnNabe1las-MBP.lan>
2018-09-07 01:47:56 +0800

    loading

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,69 +1,38 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
+let timer;
 
 Page({
     data: {
-        userInfo: {},
-        hasUserInfo: false,
-        canIUse: wx.canIUse('button.open-type.getUserInfo'),
-        dayorNight: true,
+        imgOpacity: 1,
+        iconOpacity: 0
     },
     //事件处理函数
-    bindViewTap: function() {
-        wx.navigateTo({
-            url: '../myFriend/myFriend'
-        })
-    },
-    onDay: function() {
-        this.setData({
-            dayorNight: !this.data.dayorNight
-        })
-    },
-    onNight: function() {
-        this.setData({
-            dayorNight: !this.data.dayorNight
-        })
-    },
-    toDonate: function() {
-        wx.navigateTo({
-            url: '../donate/donate'
-        })
-    },
-    toPaint:function(){
-        wx.navigateTo({
-            url: '../Paint/Paint'
-        })
-    },
-    onLoad: function() {
-        if (app.globalData.userInfo) {
-            this.setData({
-                userInfo: app.globalData.userInfo,
-                hasUserInfo: true
-            })
-        } else if (this.data.canIUse) {
-            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-            // 所以此处加入 callback 以防止这种情况
-            app.userInfoReadyCallback = res => {
-                this.setData({
-                    userInfo: res.userInfo,
-                    hasUserInfo: true
-                })
+
+    onLoad: function () {
+        // 背景逐渐透明
+        timer = setInterval(() => {
+            // 背景透明度减到0.15
+            if (this.data.imgOpacity > 0.1) {
+                this.setData({ imgOpacity: this.data.imgOpacity - 0.05 })
             }
-        } else {
-            // 在没有 open-type=getUserInfo 版本的兼容处理
-            wx.getUserInfo({
-                success: res => {
-                    app.globalData.userInfo = res.userInfo
-                    this.setData({
-                        userInfo: res.userInfo,
-                        hasUserInfo: true
-                    })
-                }
-            })
-        }
+            // 背景减到0.5时标志出现
+            if (this.data.imgOpacity < 0.5 && this.data.iconOpacity < 1) {
+                this.setData({ iconOpacity: this.data.iconOpacity + 0.025 })
+            }
+            // 标志透明度1时清除定时器
+            if (this.data.iconOpacity >= 1 && this.data.imgOpacity < 0.1) {
+                clearInterval(timer)
+            }
+        }, 150);
     },
-    getUserInfo: function(e) {
+    /**
+   * 生命周期函数--监听页面显示
+   */
+    onShow: function () {
+    },
+    getUserInfo: function (e) {
         console.log(e)
         app.globalData.userInfo = e.detail.userInfo
         this.setData({
