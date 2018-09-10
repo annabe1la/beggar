0c617e55c8b3a7e0b5365c9e491346a8c1dd8e1a
AnNabe1la <shijin@AnNabe1las-MacBook-Pro.local>
2018-09-10 22:23:39 +0800

    login

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,10 +1,15 @@
 // pages/login/login.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    userInfo: {},
+    hasUserInfo: false,
+    onShow: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
 
   },
 
@@ -12,14 +17,68 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.showToast({
+      title: '等一哈', //标题
+      icon: 'loading', //图标，支持"success"、"loading"
+      image: '', //自定义图标的本地路径，image 的优先级高于 icon
+      duration: 2000, //提示的延迟时间，单位毫秒，默认：1500
+      mask: false, //是否显示透明蒙层，防止触摸穿透，默认：false
+      success: function () {}, //接口调用成功的回调函数
+      fail: function () {}, //接口调用失败的回调函数
+      complete: function () {} //接口调用结束的回调函数
+    })
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log( app.globalData.userInfo)
+    app.globalData.userInfo = e.detail.userInfo
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  enterIn:function(){
+    wx.redirectTo({
+      url:"../main/main"
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    let t = setTimeout(
+      () => {
+        this.setData({
+          onShow: true,
+        })
+      }, 3000)
   },
 
   /**
